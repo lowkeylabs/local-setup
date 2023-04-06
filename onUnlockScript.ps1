e6fa87b5dd24d93be2d0df75e526e539ef92ba18
John Leonard <john@lowkeylabs.com>
2023-04-06 05:23:10 -0400

    Added switch for printer setup in onUnlockScript

--- onUnlockScript.ps1
+++ onUnlockScript.ps1
@@ -1,7 +1,12 @@
 # This script is run at login. It is tied to an event in the task scheduler.
 # 
 
-~\.mysetup\SetMyDefaultPrinter.ps1
+$vcu_computers = @('EGR-JL-RAK1-SRV','EGR-JL-DSK0-SRV')
+
+if ($env:COMPUTERNAME -in $vcu_computers) {
+    ~\.mysetup\SetMyDefaultPrinter.ps1
+}
+
 ~\.mysetup\FreshenWallpaper.ps1
 
 # SIG # Begin signature block
